91f61ab05ebd96d771eab2d50cbd0145207c31f5
Tirth30 <tirthpanchal11@gmail.com>
2024-04-22 17:04:18 +0530

    start script uploaded

---
+++ docker-compose-files/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define the paths to your Docker Compose files
+compose_files=(
+    "/docker-compose-files/docker-compose-jenkins.yml"
+    "/docker-compose-files/docker-compose-next-home-finder.yml"
+    "/docker-compose-files/docker-compose-website.yml"
+    "/docker-compose-files/pro-gra//docker-compose.yml"
+    # Add more paths if needed
+)
+
+# Start each Docker Compose file in the background
+for file in "${compose_files[@]}"; do
+    docker-compose -f "$file" up 
+done
+
