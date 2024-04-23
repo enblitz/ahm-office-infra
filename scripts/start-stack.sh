d968831d0569a79ae81936d1ce19b2741bffca4a
Jignesh Makwana <makwana.jignesh@gmail.com>
2024-04-23 16:44:39 +0800

    Updated script

--- scripts/start-stack.sh
+++ scripts/start-stack.sh
@@ -2,15 +2,15 @@
 
 # Define the paths to your Docker Compose files
 compose_files=(
-    "/docker-compose-files/docker-compose-jenkins.yml"
-    "/docker-compose-files/docker-compose-next-home-finder.yml"
-    "/docker-compose-files/docker-compose-website.yml"
-    "/docker-compose-files/pro-gra//docker-compose.yml"
+    "docker-compose-website.yml"
+    # "/docker-compose-files/docker-compose-next-home-finder.yml"
+    # "/docker-compose-files/docker-compose-website.yml"
+    # "/docker-compose-files/pro-gra//docker-compose.yml"
     # Add more paths if needed
 )
 
 # Start each Docker Compose file in the background
 for file in "${compose_files[@]}"; do
-    docker-compose -f "$file" up 
+    docker-compose -f "$file" up -d
 done
 
