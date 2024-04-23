bb28778b96851573c9ae45d5dca62697f1402118
Jignesh Makwana <makwana.jignesh@gmail.com>
2024-04-23 16:47:54 +0800

    Updated script

--- scripts/start-stack.sh
+++ scripts/start-stack.sh
@@ -3,6 +3,7 @@
 # Define the paths to your Docker Compose files
 compose_files=(
     "docker-compose-website.yml"
+    "docker-compose-jenkins.yml"
     # "/docker-compose-files/docker-compose-next-home-finder.yml"
     # "/docker-compose-files/docker-compose-website.yml"
     # "/docker-compose-files/pro-gra//docker-compose.yml"
