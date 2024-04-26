0cc0becb2499ea6ed83be39537f08dc0f9d40da8
makwaji <makwana.jignesh@gmail.com>
2024-04-26 22:04:06 +0800

    WIP: prometheus

--- scripts/stop-stack.sh
+++ scripts/stop-stack.sh
@@ -4,6 +4,8 @@
 compose_files=(
     "docker-compose-website.yml"
     "docker-compose-jenkins.yml"
+    "docker-compose-prometheus.yml"
+    "docker-compose-grafana.yml"
     # "/docker-compose-files/docker-compose-next-home-finder.yml"
     # "/docker-compose-files/docker-compose-website.yml"
     # "/docker-compose-files/pro-gra//docker-compose.yml"
@@ -14,4 +16,3 @@ compose_files=(
 for file in "${compose_files[@]}"; do
     docker-compose -f "$file" stop
 done
-
