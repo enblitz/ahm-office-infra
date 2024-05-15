4514667455797d69fbf6dae6ba3365501d080fb9
Jignesh Makwana <makwana.jignesh@gmail.com>
2024-05-15 12:53:59 +0800

    WIP: Obervability

--- scripts/start-stack.sh
+++ scripts/start-stack.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# Define the paths to your Docker Compose files
-compose_files=(
-    "docker-compose-website.yml"
-    "docker-compose-jenkins.yml"
-    "docker-compose-prometheus.yml"
-    "docker-compose-grafana.yml"
-    # "/docker-compose-files/docker-compose-website.yml"
-    # "/docker-compose-files/pro-gra//docker-compose.yml"
-    # Add more paths if needed
-)
-
-# Start each Docker Compose file in the background
-for file in "${compose_files[@]}"; do
-    docker compose -f "$file" up -d
-done
+docker compose up -d
